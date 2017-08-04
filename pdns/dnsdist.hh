295d78ae351d43fe15988b78f81137cd9e153437
Seth Ornstein <sornstein@globalcyberalliance.org>
2017-08-04 17:38:40 -0400

    Named Caches
    Up to commit "cleaned up source with better map",
    dated Wed Nov 22 09:36:47 2017 -0500.
    
    Squashed. dnsdist-lua2.cc additions transplanted into
    dnsdist-lua-namedcache.cc.

--- pdns/dnsdist.hh
+++ pdns/dnsdist.hh
@@ -983,6 +983,7 @@ void setPoolPolicy(pools_t& pools, const string& poolName, std::shared_ptr<Serve
 void addServerToPool(pools_t& pools, const string& poolName, std::shared_ptr<DownstreamState> server);
 void removeServerFromPool(pools_t& pools, const string& poolName, std::shared_ptr<DownstreamState> server);
 
+
 struct CarbonConfig
 {
   ComboAddress server;
@@ -1025,6 +1026,7 @@ extern GlobalStateHolder<vector<DNSDistResponseRuleAction> > g_resprulactions;
 extern GlobalStateHolder<vector<DNSDistResponseRuleAction> > g_cachehitresprulactions;
 extern GlobalStateHolder<vector<DNSDistResponseRuleAction> > g_selfansweredresprulactions;
 extern GlobalStateHolder<NetmaskGroup> g_ACL;
+
 
 extern ComboAddress g_serverControl; // not changed during runtime
 
