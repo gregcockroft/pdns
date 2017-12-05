4c4973a4e366f8f3fbaf5d44729756b1442c66ee
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2017-12-05 22:15:15 +0100

    travis: add cdb, --enable-namedcache

--- build-scripts/travis.sh
+++ build-scripts/travis.sh
@@ -392,6 +392,7 @@ install_dnsdist() {
   run 'curl "http://keyserver.ubuntu.com:11371/pks/lookup?op=get&search=0x396160EF8126A2E2" | sudo apt-key add - '
   run "sudo apt-get -qq update"
   run "sudo apt-get -qq --no-install-recommends install \
+    libcdb-dev \
     snmpd \
     libsnmp-dev \
     libfstrm-dev"
@@ -480,7 +481,8 @@ build_dnsdist(){
   run "./configure \
     ${sanitizerflags} \
     --enable-unit-tests \
-    --with-libsodium \
+    --enable-namedcache \
+    --enable-libsodium \
     --enable-dnscrypt \
     --enable-dns-over-tls \
     --enable-dnstap \
