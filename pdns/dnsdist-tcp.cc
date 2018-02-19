4229908da9ca473debfccbbc5b885fe240b7dc39
Chris Hofstaedtler <chris.hofstaedtler@deduktiva.com>
2018-02-19 14:50:00 +0100

    Follow QTag cleanup from 15fac047e6f34f3abd1ba4fbc3e239fa8be52fcc upstream

--- pdns/dnsdist-tcp.cc
+++ pdns/dnsdist-tcp.cc
@@ -995,6 +995,10 @@ static void handleDownstreamIO(std::shared_ptr<IncomingTCPConnectionState>& stat
   else {
     handleNewIOState(state, iostate, fd, handleDownstreamIOCallback, iostate == IOState::NeedRead ? state->getBackendReadTTD(now) : state->getBackendWriteTTD(now));
   }
+#ifdef HAVE_PROTOBUF
+        dr.uniqueId = dq.uniqueId;
+#endif
+        dr.qTag = dq.qTag;
 
   if (connectionDied) {
     sendQueryToBackend(state, now);
